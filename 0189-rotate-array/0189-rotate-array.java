3f06c1b3ed1e8f7d05caca9138aed3e9de77c9f6
Krishna Chhikara <119605821+krishnachhkara@users.noreply.github.com>
2026-06-26 14:56:48 +0530

    Time: 186 ms (3.63%), Space: 61.5 MB (29.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -15,18 +15,41 @@ class Solution {
         // }
 
         //better version
-        int n = arr.length;
-        int[] ans = new int[n];
-        for(int i =0 ; i<n;i++){
-            int newIndex = (i+k)%n;
-            ans[newIndex] = arr[i];
-        }
+        // int n = arr.length;
+        // int[] ans = new int[n];
+        // for(int i =0 ; i<n;i++){
+        //     int newIndex = (i+k)%n;
+        //     ans[newIndex] = arr[i];
+        // }
 
-        for(int i = 0;i<n;i++){
-            arr[i] = ans[i];
-        }
+        // for(int i = 0;i<n;i++){
+        //     arr[i] = ans[i];
+        // }
+        // for(int i =0;i<n;i++){
+        //     System.out.print(arr[i]);
+        // }
+
+        //optimal approach
+        int n = arr.length;
+        k = k%n;
+
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
         for(int i =0;i<n;i++){
             System.out.print(arr[i]);
         }
+
+        
     }
+    static int[] reverse(int [] arr,int start,int end){
+            while(start<end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+            return arr;
+        }
 }
