77d334afbc50c976784de849632bdb86db25f54c
Krishna Chhikara <119605821+krishnachhkara@users.noreply.github.com>
2026-09-15 21:53:51 +0530

    Time: 2 ms (99.29%), Space: 44.3 MB (62.9%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0,
+            j = s.length() - 1;
+
+        while(i<j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if(!Character.isLetterOrDigit(left)){
+                i = i+1;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(right)){
+                j = j - 1;
+                continue;
+            }
+
+            if(Character.toLowerCase(left) != Character.toLowerCase(right)){
+                return false;
+            }
+
+            i = i + 1;
+            j = j - 1;
+        }    
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
