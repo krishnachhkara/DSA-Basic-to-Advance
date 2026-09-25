d746548b4604e9bc147578c843eb104e7467db22
Krishna Chhikara <119605821+krishnachhkara@users.noreply.github.com>
2026-09-25 20:27:59 +0530

    Time: 18 ms (77.09%), Space: 108.2 MB (38.07%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for( int i = 0; i < nums.length; i++){
+
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
