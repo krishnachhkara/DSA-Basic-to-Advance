d86dc122fb8028b79d904fcfa36829aaa0bc1ca7
Krishna Chhikara <119605821+krishnachhkara@users.noreply.github.com>
2026-09-26 14:31:54 +0530

    Time: 31 ms (59.31%), Space: 95.5 MB (61.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,31 +1,84 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        
+        int length = 0;
+        int maxLen = 0;
 
-        Arrays.sort(nums);
-        int currentCount = 1;
-        int maxCount = 1;
-
-        if(nums.length ==  0){
-            return maxCount = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int a: nums){
+            set.add(a);
         }
 
-        for(int i = 1 ; i<nums.length ; i++){
-            if(nums[i] == nums[i-1]){
+        for(int elem:set){
+
+            if(set.contains(elem-1)){
                 continue;
             }
-
-            else if(nums[i] == nums[i-1] + 1){
-                currentCount++;
-            }
-
             else{
-                maxCount = Math.max(currentCount,maxCount);
-                currentCount = 1;
+                length += 1;
+                while(set.contains(elem+1)){
+                    elem++;
+                    length++;
+                }
+                maxLen = Math.max(maxLen,length);
+                length = 0;
             }
-        }
-        maxCount = Math.max(maxCount, currentCount);
 
-        return maxCount;
+        }
+
+        return maxLen;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // own brute force sol
+        // Arrays.sort(nums);
+        // int currentCount = 1;
+        // int maxCount = 1;
+
+        // if(nums.length ==  0){
+        //     return maxCount = 0;
+        // }
+
+        // for(int i = 1 ; i<nums.length ; i++){
+        //     if(nums[i] == nums[i-1]){
+        //         continue;
+        //     }
+
+        //     else if(nums[i] == nums[i-1] + 1){
+        //         currentCount++;
+        //     }
+
+        //     else{
+        //         maxCount = Math.max(currentCount,maxCount);
+        //         currentCount = 1;
+        //     }
+        // }
+        // maxCount = Math.max(maxCount, currentCount);
+
+        // return maxCount;
         
     }
 }
